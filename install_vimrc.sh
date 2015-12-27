068f43ebb7e12e56babc0eedd2a7dfd55540844d
Yihang Yang <goat@kanjian.com>
2015-12-27 16:52:19 +0000

    fix typo pwd

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-ln -s ${pwd}/local_vimrc ~/.vimrc
+ln -s `pwd`/local_vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
